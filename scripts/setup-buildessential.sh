a1c02b444756f2193eed860ffac712813dc2949b
Andrey Antukh <niwi@niwi.be>
2014-09-24 14:36:11 +0200

    Remove unused line.

--- scripts/setup-buildessential.sh
+++ scripts/setup-buildessential.sh
@@ -1,4 +1,3 @@
 apt-install-if-needed build-essential binutils-doc autoconf flex bison
 apt-install-if-needed libjpeg-dev libfreetype6-dev zlib1g-dev
 apt-install-if-needed libgdbm-dev libncurses5-dev automake libtool libffi-dev curl
-apt-install-if-needed libreadline6-dev, libyaml-dev, libsqlite3-dev, sqlite3, pkg-config
