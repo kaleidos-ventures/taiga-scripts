875732776fa93903819eadc0892a998234c60c45
Andrey Antukh <niwi@niwi.be>
2014-09-24 18:52:21 +0200

    Remove duplicated code.

--- setup-server.sh
+++ setup-server.sh
@@ -1,44 +1,5 @@
 #!/bin/bash
-
-rootdir=`pwd`
-username="$USER"
-
-echo -n "Scheme (default http): "
-read scheme
-echo -n "Hostname (default: localhost:8000): "
-read hostname
-
-if [ -z "$hostname" ]; then
-    hostname="localhost:8000"
-fi
-
-if [ -z "$scheme" ]; then
-    scheme="http"
-fi
-
-pushd ~
-mkdir -p logs
-mkdir -p conf
-popd
-
-# Bootstrap
-source ./scripts/setup-config.sh
-source ./scripts/setup-apt.sh
-
-# Setup and install services dependencies
-source ./scripts/setup-postgresql.sh
-source ./scripts/setup-redis.sh
-source ./scripts/setup-rabbitmq.sh
-
-# Setup and install python related dependencies
-source ./scripts/setup-buildessential.sh
-source ./scripts/setup-ruby.sh
-source ./scripts/setup-nodejs.sh
-source ./scripts/setup-python.sh
-
-# Setup Taiga
-source ./scripts/setup-frontend.sh
-source ./scripts/setup-backend.sh
+source ./setup-devel.sh
 
 # Post Setup Services
 source ./scripts/setup-circus.sh
