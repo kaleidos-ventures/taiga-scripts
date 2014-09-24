167a875603c5a51fdf9ca8556a6b08f408ea0346
Andrey Antukh <niwi@niwi.be>
2014-09-24 17:04:27 +0200

    Add missing sudo prefix on first run circusd.

--- scripts/setup-circus.sh
+++ scripts/setup-circus.sh
@@ -47,4 +47,4 @@ EOF
 sudo mv /tmp/rc.local /etc/rc.local
 sudo chmod +x /etc/rc.local
 
-/usr/local/bin/circusd --daemon /home/${username}/conf/circus.ini
+sudo /usr/local/bin/circusd --daemon /home/${username}/conf/circus.ini
