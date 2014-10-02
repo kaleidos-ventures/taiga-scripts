08e965e625848ad4dba942d395567b01ee6ffd81
Andrey Antukh <niwi@niwi.be>
2014-10-02 13:58:17 +0200

    Add development scripts.

--- scripts/setup-config.sh
+++ scripts/setup-config.sh
@@ -28,3 +28,43 @@ set incsearch
 set fileencoding=utf8
 set encoding=utf8
 EOF
+
+cat >> ~/.tmux-conf.sh <<EOF
+function taiga-runserver {
+    session=taiga
+    state=$(tmux ls 2>/dev/null)
+    if $(echo $state | grep -q "$session"); then
+        if $(echo $state | grep -qv "(attached)"); then
+            tmux attach -t $session
+        fi
+        tmux select-window -t servers
+    else
+        tmux new-session -ds $session -n servers
+        tmux send-keys -t $session 'taiga-runserver-front' C-m
+        tmux split-window -t $session
+        tmux send-keys -t $session 'taiga-runserver-back' C-m
+        tmux attach -t $session
+    fi
+}
+
+function taiga-runserver-front {
+    workon taiga
+    cd ~/taiga-front
+    gulp
+}
+
+function taiga-runserver-back {
+    circusctl stop taiga
+    workon taiga
+    cd ~/taiga-back
+    python manage.py runserver 0.0.0.0:8001
+}
+EOF
+
+cat >> ~/.bash_profile <<EOF
+source ~/.tmux-conf.sh
+EOF
+
+source ~/.tmux-conf.sh
+
+
