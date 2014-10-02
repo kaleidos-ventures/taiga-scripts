340343ae2f56cd023983b9d296097d9176dc4be5
Andrey Antukh <niwi@niwi.be>
2014-10-02 15:27:27 +0200

    Fix $ char escaping.

--- scripts/setup-config.sh
+++ scripts/setup-config.sh
@@ -33,17 +33,17 @@ cat > ~/.tmux-conf.sh <<EOF
 function taiga-runserver {
     session=taiga
     state=\$(tmux ls 2>/dev/null)
-    if \$(echo $state | grep -q "$session"); then
-        if \$(echo $state | grep -qv "(attached)"); then
+    if \$(echo $state | grep -q "\$session"); then
+        if \$(echo \$state | grep -qv "(attached)"); then
             tmux attach -t $session
         fi
         tmux select-window -t servers
     else
-        tmux new-session -ds $session -n servers
-        tmux send-keys -t $session 'taiga-runserver-front' C-m
-        tmux split-window -t $session
-        tmux send-keys -t $session 'taiga-runserver-back' C-m
-        tmux attach -t $session
+        tmux new-session -ds \$session -n servers
+        tmux send-keys -t \$session 'taiga-runserver-front' C-m
+        tmux split-window -t \$session
+        tmux send-keys -t \$session 'taiga-runserver-back' C-m
+        tmux attach -t \$session
     fi
 }
 
