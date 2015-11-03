09003c3cf089aa5a57acde73de987adb05033055
David Barragán Merino <david.barragan@kaleidos.net>
2015-11-03 16:41:52 +0100

    Remove TEMPLATE_DUBUG, django 1.8 doesn't need it

--- scripts/setup-backend.sh
+++ scripts/setup-backend.sh
@@ -8,15 +8,13 @@ cat > /tmp/settings.py <<EOF
 from .common import *
 
 MEDIA_URL = "/media/"
-STATIC_URL = "static/"
-ADMIN_MEDIA_PREFIX = "/static/admin/"
+STATIC_URL = "/static/"
 
 # This should change if you want generate urls in emails
 # for external dns.
 SITES["front"]["domain"] = "localhost:8000"
 
 DEBUG = True
-TEMPLATE_DEBUG = True
 PUBLIC_REGISTER_ENABLED = True
 
 DEFAULT_FROM_EMAIL = "no-reply@example.com"
