b4071d8da81e88fb9d6b1b2908099c7c2d7af044
Matheus Vieira <eu@matheusvieira.info>
2016-05-12 13:03:56 -0300

    Update setup-postgresql.sh

--- scripts/setup-postgresql.sh
+++ scripts/setup-postgresql.sh
@@ -13,8 +13,8 @@ function dropdb-if-needed {
 }
 
 if [ ! -e ~/.setup/postgresql ]; then
-    apt-install-if-needed postgresql-9.3 postgresql-contrib-9.3 \
-        postgresql-doc-9.3 postgresql-server-dev-9.3
+    apt-install-if-needed postgresql-9.5 postgresql-contrib-9.5 \
+        postgresql-doc-9.5 postgresql-server-dev-9.5
 
     sudo -u postgres createuser --superuser $USER &> /dev/null
     sudo -u postgres createdb $USER &> /dev/null
