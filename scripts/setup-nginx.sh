4161843d41528e45af28dfbd3622d0ec7a9d4b83
Yuxuan Zhao <yzhao9@ualberta.ca>
2019-04-15 10:04:11 -0600

    add header to prevent clickjacking

--- scripts/setup-nginx.sh
+++ scripts/setup-nginx.sh
@@ -27,6 +27,7 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_pass http://127.0.0.1:8001/api;
         proxy_redirect off;
+        add_header X-Frame-Options "SAMEORIGIN" always;
     }
     
     # Django admin access (/admin/)
